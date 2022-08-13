08da8dfce3d27f05b58eb7d72efb621e229cbafd
Pedro Garrido <belcaik@gmail.com>
2022-08-13 18:29:27 -0400

    finish

--- index.js
+++ index.js
@@ -23,23 +23,29 @@ app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
-app.get("/api/:time", (req, res) => {
-  let time = req.params.time;
+app.get("/api/:date?", (req, res) => {
+  let time = req.params.date;
 
-  if (time.match(/[0-9]{13}/)) {
-    let intd = parseInt(time);
+  if (!time) {
+    let now = new Date();
     res.json({
-      unix: time,
-      utc: new Date(intd).toUTCString()
+      unix: now.getTime(),
+      utc: now.toUTCString(),
     });
-  }else if (time.match(/[0-9]{4}\-[0-9]{2}\-[0-9]{2}/)) {
+  } else if (time.match(/[0-9]{4}\-[0-9]{2}\-[0-9]{2}/)) {
     res.json({
       unix: new Date(time).getTime(),
-      utc: new Date(time).toUTCString()
+      utc: new Date(time).toUTCString(),
+    });
+  } else if (time.match(/[0-9]{13}/)) {
+    let intd = parseInt(time);
+    res.json({
+      unix: intd,
+      utc: new Date(intd).toUTCString(),
     });
   } else {
     res.json({
-      error: "Invalid Date"
+      error: "Invalid Date",
     });
   }
 });
